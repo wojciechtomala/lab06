07f47cd094be334e3328bd1b7ea7960c785b9a7c
Wojciech Tomala <105435994+wojciechtomala@users.noreply.github.com>
2023-10-25 15:24:45 +0200

    Add files via upload

---
+++ Online_Store/Shop/Models/ShopItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop.Models
+{
+    public class ShopItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string imgURL { get; set; }
+    }
+}
