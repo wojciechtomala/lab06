4d31dd0b1071be31694305dd57f223d5f6a1df7c
Wojciech Tomala <105435994+wojciechtomala@users.noreply.github.com>
2023-11-04 18:05:56 +0100

    Add files via upload

---
+++ RegisterForm/Controllers/DaneController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using zajecia3.Models;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+// klasa kontrolera - kontroler
+// model - zwykła klasa
+// widok - pusty widok razor
+namespace zajecia3.Controllers
+{
+    public class DaneController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Form() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else return View();
+
+        }
+        public IActionResult Wynik(Dane dane) {
+            return View(dane);
+        }
+    }
+}
---
+++ RegisterForm/Models/Dane.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace zajecia3.Models
+{
+    public class Dane
+    {
+        // NAME:
+        [Required(ErrorMessage = "Podaj Imie")]
+        [MinLength(2, ErrorMessage = "Imie musi zawierać co najmniej 2 znaki")]
+        public string Imie { get; set; }
+        
+        // SURNAME:
+        [Required(ErrorMessage = "Podaj Nazwisko")]
+        [MinLength(2, ErrorMessage = "Nazwisko musi zawierać co najmniej 2 znaki")]
+        public string Nazwisko { get; set; }
+
+
+        // EMAIL:
+        [Required(ErrorMessage = "Wymagany poprawny adres email: example@domain.com")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        public string Email { get; set; }
+        
+        // PASSWORD:
+        [Required(ErrorMessage = "Podaj Hasło")]
+        [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$", ErrorMessage = "Hasło musi zawierać co najmniej jedną cyfrę, jedną dużą literę i jedną małą literę")]
+        [DataType(DataType.Password)]
+        public string Haslo { get; set; }
+
+        // REPEAT PASSWORD:
+        [Required(ErrorMessage = "Potwierdź Hasło")]
+        [Compare("Haslo", ErrorMessage = "Hasło i jego potwierdzenie muszą być takie same")]
+        [DataType(DataType.Password)]
+        public string PotwierdzenieHasla { get; set; }
+
+        // PHONE:
+        [Required(ErrorMessage = "Podaj Nr telefonu")]
+        [DataType(DataType.PhoneNumber)]
+        public string NrTelefonu { get; set; }
+
+        // AGE:
+        [Required(ErrorMessage = "Podaj Wiek")]
+        [Range(11, 80, ErrorMessage = "Wiek musi być pomiędzy 11 a 80 lat")]
+        public int Wiek { get; set; }
+
+        // CITY:
+        [Required(ErrorMessage = "Wybierz Miasto")]
+        [EnumDataType(typeof(Miasto), ErrorMessage = "Wybrane miasto nie jest dostępne")]
+        public Miasto WybraneMiasto { get; set; }
+        public enum Miasto
+        {
+            [Display(Name = "Warszawa")]
+            Warszawa,
+
+            [Display(Name = "Kraków")]
+            Krakow,
+
+            [Display(Name = "Poznań")]
+            Poznan,
+
+            [Display(Name = "Wrocław")]
+            Wroclaw,
+
+            [Display(Name = "Gdańsk")]
+            Gdansk
+        }
+
+        /*
+        [Required(ErrorMessage = "Prosze podaj Temat")]
+        public string Temat { get; set; }
+        [Required(ErrorMessage = "Prosze podaj Tresc")]
+        [MinLength(10), MaxLength(50)]
+        public string Tresc { get; set; }
+        */
+    }
+}
